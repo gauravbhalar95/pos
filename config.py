b3376b4aa9c75aaae7a86c9c2b63375bbb0d6804
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2026-07-07 18:51:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+from dotenv import *
+import os
+
+
+load_dotenv()
+
+root2 = os.getenv("root")
+user2 = os.getenv("user")
+GAURAV = os.getenv("password")
+website = os.getenv("database")
+port = os.getenv("port")
+
